f1124e953f31599f4dfe924297096d637a3c99ea
Titli Purkait <titlipurkait268@gmail.com>
2025-07-07 21:45:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Change navbar style on scroll
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  navbar.classList.toggle('scrolled', window.scrollY > 30);
+});
+
